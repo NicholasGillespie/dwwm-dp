a58d90be97b150211d4f1629bce742705d9aec34
Nicholas Gillespie <contact@nicholasgillespie.com>
2022-03-22 12:25:05 +0100

    adjust header

--- includes/header.php
+++ includes/header.php
@@ -5,16 +5,17 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>My blog</title>
+  <title>DWWM Project</title>
+  <meta name="author" content="Nicholas Gillespie">
+  <meta name="description" content="End of course project. A blog in CRUD format.">
+  <meta name="keywords" content="dwwm, blog, crud, ENDEX">
   <link rel="stylesheet" href="/css/root.css">
   <link rel="stylesheet" href="/css/global.css">
   <link rel="stylesheet" href="/css/composition.css">
   <link rel="stylesheet" href="/css/utility.css">
-
 </head>
 
 <body>
-
   <header role="banner" class="[ cluster ][ justify:space-between ]">
     <a href="/"><img src="" alt="logo"></a>
     <nav>
