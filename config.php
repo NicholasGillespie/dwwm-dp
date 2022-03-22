aa5327e7281fed1060162ee1e7d95ad5d0cb5909
Nicholas Gillespie <contact@nicholasgillespie.com>
2022-03-22 12:27:06 +0100

    add config file to github

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+// database
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'dwwm_realisation_dp');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+
+// SMTP
+define('SMTP_HOST', 'smtp.gmail.com');
+define('SMTP_SECURE', 'tls');
+define('SMTP_PORT', '587');
+define('SMTP_USER', 'username@gmail.com');
+define('SMTP_PASS', 'password');
+
+
+// Error msg -> dev 'true' / prod 'false'
+define('SHOW_ERROR_DETAIL', false);
